bef8e16c5d220bd22412e2b0b10bf2eeb44a974e
Deon Leer <deon.leer96@gmail.com>
2021-04-21 19:41:00 -0700

    moved imports

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
 //imports
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
+
 /// <reference path="components/project-list.ts" />
 /// <reference path="components/project-input.ts" />
 namespace App {
