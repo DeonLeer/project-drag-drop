d85fff5543f0a6dda455e6290f9a6f8afa53d2a5
Deon Leer <deon.leer96@gmail.com>
2021-04-21 19:37:45 -0700

    finished re-organizing

---
+++ src/components/project-item.ts
@@ -0,0 +1,38 @@
+/// <reference path="base-component.ts" />
+namespace App {
+  // Project Item Class
+  export class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id);
+      this.project = project;
+      this.configure();
+      this.renderContent();
+    }
+    @AutoBind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData("text/plain", this.project.id);
+      event.dataTransfer!.effectAllowed = "move";
+    }
+
+    dragEndHandler(_: DragEvent) {
+      console.log("DragEnd");
+    }
+
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler);
+      this.element.addEventListener("dragend", this.dragEndHandler);
+    }
+    renderContent() {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector(
+        "h3"
+      )!.textContent = `${this.project.people.toString()} ${
+        this.project.people === 1 ? "person" : "people"
+      } assigned`;
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+}
